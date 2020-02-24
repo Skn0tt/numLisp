33fc4e985b765ad5e43b17bf35e0d154a8bc789c
Simon Knott <info@simonknott.de>
2020-02-24 15:00:27 +0100

    Implement AST generator

--- src/ASTGenerator.spec.ts
+++ src/ASTGenerator.spec.ts
@@ -1,9 +1,44 @@
-import { generateAST } from "./ASTGenerator"
+import { generateAST, untilClosingBracket, withoutEnclosingBrackets } from "./ASTGenerator"
 
 describe("ASTGenerator", () => {
+
   test.each`
-    given | expected
-    ${["(", "list", "a", "b", "c", ")"]} | ${[["list", "a", "b", "c"]]}
+    given                                | expected
+    ${["(", "list", "a", "b", "c", ")"]} | ${["(", "list", "a", "b", "c", ")"]}
+    ${["(", "cons", "b", "c", ")", ")"]} | ${["(", "cons", "b", "c", ")"]}
+  `("untilClosingBracket($given) ~= $expected", ({ given, expected }) => {
+    expect(untilClosingBracket(given)).toEqual(expected);
+  })
+
+  describe("withoutEnclosingBrackets", () => {
+    describe("when giving enclosing brackets", () => {
+      it("return the inner part", () => {
+        expect(
+          withoutEnclosingBrackets(["(", "a", ")"])
+        ).toEqual(["a"]);
+      })
+    })
+
+    describe("when starting with smth else", () => {
+      it("doesnt do anything", () => {
+        expect(
+          withoutEnclosingBrackets(["b", "a", ")"])
+        ).toEqual(["b", "a", ")"]);
+      });
+    })
+
+    describe("when ending with smth else", () => {
+      it("doesnt do anything", () => {
+        expect(
+          withoutEnclosingBrackets(["(", "a", "c"])
+        ).toEqual(["(", "a", "c"]);
+      })
+    })
+  })
+
+  test.each`
+    given                                                  | expected
+    ${["(", "list", "a", "b", "c", ")"]}                   | ${[["list", "a", "b", "c"]]}
     ${["(", "cons", "a", "(", "cons", "b", "c", ")", ")"]} | ${[["cons", "a", ["cons", "b", "c"]]]}
   `("generateAST($given) ~= $expected", ({ given, expected }) => {
     expect(generateAST(given)).toEqual(expected);
--- src/ASTGenerator.ts
+++ src/ASTGenerator.ts
@@ -1,7 +1,67 @@
+import * as _ from "lodash";
+
+const indexOfClosingBracket = (tokens: string[]): number => {
+  let openedBrackets: number = 0;
+
+  for (let i = 0; i < tokens.length; i++) {
+    const isFirstToken = i === 0;
+    const token = tokens[i];
+
+    switch (token) {
+      case "(":
+          openedBrackets++;
+          break;
+      case ")":
+        openedBrackets--;
+        if (!isFirstToken && openedBrackets === 0) {
+          return i;
+        }
+        break;
+    }
+  }
+
+  return -1;
+}
+
+export const untilClosingBracket = (tokens: string[]): string[] => {
+  const closingIndex = indexOfClosingBracket(tokens);
+  return tokens.slice(0, closingIndex + 1);
+}
+
+export const withoutEnclosingBrackets = (tokens: string[]): string[] => {
+  if (_.first(tokens) === "(" && _.last(tokens) === ")") {
+    return _.tail(_.initial(tokens));
+  }
+
+  return tokens;
+}
+
 type AST = (string | AST)[];
 
-export const generateAST = (tokens: string[]) => {
+export const generateAST = (tokens: string[]): AST => {
   const result: AST = [];
 
-  
+  for (let i = 0; i < tokens.length; i++) {
+    const token = tokens[i];
+
+    switch (token) {
+      case "(":
+        const remainder = tokens.slice(i);
+        const lengthOfSubExpression = indexOfClosingBracket(remainder);
+        const endOfSubExpression = i + lengthOfSubExpression;
+        
+        const subExpressionWithEnclosingBrackets = tokens.slice(i, endOfSubExpression + 1);
+        const subExpression = withoutEnclosingBrackets(subExpressionWithEnclosingBrackets);
+
+        i = endOfSubExpression;
+        result.push(generateAST(subExpression));
+
+        break;
+      default:
+        result.push(token);
+        break;
+    }
+  }
+
+  return result;
 }
